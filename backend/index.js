b57a3263e0a4dd62424d8d16cfe1a9dca7b3e5f6
rodrigo.bisterco <rodrigo.bisterco@gmail.com>
2020-03-23 16:55:15 -0300

    ajuste lap win

--- backend/index.js
+++ backend/index.js
@@ -6,7 +6,8 @@ app.get('/', (request, response) => {
     return response.json({ 
         evento: 'Semana OmniStack 11.0',
         aluno: 'Rodrigo Bisterço',
-        data: '2020-03-23'})
+        data: '2020-03-23',
+        comment: "VS lap Win"})
 })
 
 app.listen(3333);
