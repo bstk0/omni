17d099ab2d9b02576923c0a7a124e619fa5d672c
rodrigo.bisterco <rodrigo.bisterco@gmail.com>
2020-03-24 11:49:42 -0300

    Query Params

--- backend/index.js
+++ backend/index.js
@@ -2,12 +2,20 @@ const express = require('express');
 
 const app = express();
 
-app.get('/', (request, response) => {
-    return response.json({ 
+
+//app.get('/', (request, response) => {
+
+app.get('/users', (request, response) => {
+    const params = request.query;
+
+    console.log(params);
+
+    return response.json({
         evento: 'Semana OmniStack 11.0',
         aluno: 'Rodrigo Bisterço',
         data: '2020-03-23',
-        comment: "VS lap Win"})
+        comment: "VS lap Win"
+    })
 })
 
 app.listen(3333);
