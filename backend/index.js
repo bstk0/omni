5c65e047a3c7f3fb62a6103ffad40e6ce5ca7d23
rodrigo.bisterco <rodrigo.bisterco@gmail.com>
2020-03-24 11:58:43 -0300

    Request Body

--- backend/index.js
+++ backend/index.js
@@ -2,13 +2,14 @@ const express = require('express');
 
 const app = express();
 
+app.use(express.json());
 
 //app.get('/', (request, response) => {
 
-app.get('/users/:id', (request, response) => {
-    const params = request.params;
+app.post('/users', (request, response) => {
+    const body = request.body;
 
-    console.log(params);
+    console.log(body);
 
     return response.json({
         evento: 'Semana OmniStack 11.0',
